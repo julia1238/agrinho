388e0824f3d389d6536916be50302d8eb8addeca
julia1238 <julia.roxo.candido@escola.pr.gov.br>
2024-07-24 07:47:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function comprar(){
+
+     const width = 800;
+     const heigth = 600;
+     const left = (screen.width/2) - (width/2);
+     const top = (screen.height/2) - (heigth/2);
+ window.open('pagamento.html','Pagamento', 'width=${width},heigth=${heigth},top=${top},left=${left}');
+}
